640ae97444eeb5e9b4285c3c04af312dd637b7d9
kirthikasiva <kirthika.b1995@gmail.com>
2026-03-17 21:57:54 +0530

    Add files via upload
    Day1 Assignment

---
+++ day1Assignment.js
@@ -0,0 +1,37 @@
+// // Declare variables using `let` for different data types in JavaScript, and verify their types using `typeof`,
+// including an uninitialized variable.
+// Assignment Requirements:
+// userName // String
+// organization // String
+// contactNumber // Number
+// knowsAutomation // Boolean
+// usesPlaywright // Undefined (not assigned) 
+
+// userName // String
+
+let username ="Kirthika";
+console.log(username)
+console.log(typeof username )
+
+// organization // String
+let organization="company";
+console.log(organization)
+console.log(typeof organization)
+
+// contactNumber // Number
+let contactNumber=1235677
+console.log(contactNumber)
+console.log(typeof contactNumber)
+
+//// knowsAutomation // Boolean
+
+let knowsAutomation=true
+console.log(knowsAutomation)
+console.log(typeof knowsAutomation)
+
+// usesPlaywright // Undefined (not assigned) 
+let usesPlaywright
+console.log(usesPlaywright)
+console.log(typeof usesPlaywright)
+
+
